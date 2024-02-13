fa1754dcb3e52c6a97da5c40b3dc99c2e5817b21
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-13 17:30:21 -0500

    change default shell instead of sourcing

--- install.sh
+++ install.sh
@@ -9,23 +9,23 @@ if [[ $OSTYPE == "linux-gnu"* ]]; then
 fi
 
 # Install oh-my-zsh
-echo installing oh-my-zsh
+echo INSTALLING OH-MY-ZSH
 sh -c "RUNZSH='no' KEEP_ZSHRC='yes' $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install powerlevel10k
-echo installing p10k
+echo INSTALLING POWERLEVEL10K
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 # Install zsh-syntax-highlighting
-echo installing zsh-syntax-highlighting
+echo INSTALLING ZSH-SYNTAX-HIGHLIGHTING 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
 # Install zsh-autocomplete
-echo installing zsh-autocomplete
+echo INSTALLING ZSH-AUTOCOMPLETE
 git clone https://github.com/marlonrichert/zsh-autocomplete.git "$ZSH_CUSTOM/plugins/zsh-autocomplete"
 
 # Install lsd
-echo "installing lsd (with apt or homebrew)"
+echo "INSTALLING LSD"
 if [[ $OSTYPE == "linux-gnu"* ]]; then
     apt install lsd
 elif [[ $OSTYPE == "darwin"* ]]; then
@@ -34,9 +34,5 @@ else
     echo "cannot detect OS as linux or macOS, please manually install lsd instead"
 fi
 
-# Source .zshrc
-echo sourcing .zshrc
-# shellcheck source=/dev/null
-source "$HOME/.zshrc"
-
+chsh
 echo COMPELTE
