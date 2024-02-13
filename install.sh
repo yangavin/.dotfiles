210dcfe2abaa7b8eadcd1fc8ac676f7e33dd6689
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-13 15:19:56 -0500

    add installation of plugins

--- install.sh
+++ install.sh
@@ -4,4 +4,14 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+
+# Install zsh-autocomplete
+git clone https://github.com/marlonrichert/zsh-autocomplete.git "$ZSH_CUSTOM/plugins/zsh-autocomplete"
+
+# Source .zshrc
+# shellcheck source=/dev/null
+source "$HOME/.zshrc"
