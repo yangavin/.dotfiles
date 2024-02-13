0d9f1926ae628c3fba0603a070f6fcc4353a7498
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-13 15:56:03 -0500

    echo what is being done for each command

--- install.sh
+++ install.sh
@@ -3,17 +3,24 @@
 ln -fs "$(pwd)"/config/.* "$HOME"/
 
 # Install oh-my-zsh
+echo installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install powerlevel10k
+echo installing p10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 # Install zsh-syntax-highlighting
+echo installing zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
 # Install zsh-autocomplete
+echo installing zsh-autocomplete
 git clone https://github.com/marlonrichert/zsh-autocomplete.git "$ZSH_CUSTOM/plugins/zsh-autocomplete"
 
 # Source .zshrc
+echo sourcing .zshrc
 # shellcheck source=/dev/null
 source "$HOME/.zshrc"
+
+echo COMPELTE
