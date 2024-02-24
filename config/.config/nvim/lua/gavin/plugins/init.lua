9b536cfc90306d205ed3c84029d4ccb95e63aae6
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-24 15:46:07 -0500

    remove background (allowing for transparency)

--- config/.config/nvim/lua/gavin/plugins/init.lua
+++ config/.config/nvim/lua/gavin/plugins/init.lua
@@ -2,7 +2,13 @@
 return {{
     'projekt0n/github-nvim-theme',
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
+    opts = {
+        transparent = true
+    },
+    config = function(_, opts)
+        require('github-theme').setup({
+            options = opts
+        })
         vim.cmd("colorscheme github_dark_default")
     end
 }}
