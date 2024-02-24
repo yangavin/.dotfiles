e75fe30e369efa8223056c656cb054d567cd35f2
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-23 23:38:26 -0500

    properly setup lazy vim for plugin management (added telescope as well)

--- config/.config/nvim/lua/gavin/init.lua
+++ config/.config/nvim/lua/gavin/init.lua
@@ -1,22 +1,3 @@
+require("gavin.lazy")
 require("gavin.options")
 require("gavin.keymaps")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
-})
--- config/.config/nvim/lua/gavin/keymaps.lua
+++ config/.config/nvim/lua/gavin/keymaps.lua
@@ -1,9 +1,6 @@
-vim.g.mapleader = " "
-
 local map = vim.keymap.set
 -- Easily source configuration
 map("n", "<leader><leader>", ":so<CR>")
 
 -- Project view
 map("n", "<leader>pv", ":Ex<CR>")
-
