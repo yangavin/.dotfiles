34afc0208e30b2406a0f1dd2b6d23ac9c5e8877c
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-24 12:52:33 -0500

    change grep files to <leader>gf

--- config/.config/nvim/lua/gavin/plugins.lua
+++ config/.config/nvim/lua/gavin/plugins.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>gf', builtin.live_grep, {})
         vim.keymap.set('n', '<C-f>', builtin.find_files, {})
     end
 }
