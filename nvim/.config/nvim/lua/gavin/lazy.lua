c1e1f6b2c99ada2091dfb51fb08b31d8af36a1b4
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-08 23:19:33 -0500

    disable notification when config is changed
    sorry for the extra formatting diffs :P

--- nvim/.config/nvim/lua/gavin/lazy.lua
+++ nvim/.config/nvim/lua/gavin/lazy.lua
@@ -4,17 +4,21 @@ vim.g.mapleader = " "
 -- Initializing lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
-require("lazy").setup("gavin.plugins")
-
+require("lazy").setup("gavin.plugins", {
+	change_detection = {
+		notify = false,
+	},
+	colorscheme = { "github-theme" },
+})
