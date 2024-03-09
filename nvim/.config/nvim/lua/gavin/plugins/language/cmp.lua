223a1424a787cc78a6981f18754d9e406a0760e2
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-09 01:51:25 -0500

    add snippet jumping key mappings

--- nvim/.config/nvim/lua/gavin/plugins/language/cmp.lua
+++ nvim/.config/nvim/lua/gavin/plugins/language/cmp.lua
@@ -9,30 +9,40 @@ return {
 	},
 	config = function()
 		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
 		cmp.setup({
 			snippet = {
 				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
-					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					luasnip.lsp_expand(args.body) -- For `luasnip` users.
 				end,
 			},
 			window = {
 				completion = cmp.config.window.bordered(),
 				-- documentation = cmp.config.window.bordered(),
 			},
-			mapping = cmp.mapping.preset.insert({
-				-- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-				-- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+			mapping = {
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({
 					select = true,
 				}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				["<Tab>"] = cmp.mapping.confirm({
-					select = true,
-				}),
-			}),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if luasnip.expand_or_jumpable() then
+						luasnip.jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			},
 			sources = cmp.config.sources(
 				{ {
 					name = "nvim_lsp",
