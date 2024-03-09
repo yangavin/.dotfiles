7a972cefa796141b8246c3d2cde3ad27200cbbc5
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-08 23:08:57 -0500

    format conform.lua

--- nvim/.config/nvim/lua/gavin/plugins/conform.lua
+++ nvim/.config/nvim/lua/gavin/plugins/conform.lua
@@ -1,22 +1,23 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {
-        formatters_by_ft = {
-            lua = {"stylua"},
-            python = {"black"},
-            html = {"prettier"},
-            css = {"prettier"},
-            javascript = {"prettier"},
-            javascriptreact = {"prettier"},
-            typescript = {"prettier"},
-            typescriptreact = {"prettier"},
-            json = {"prettier"},
-            markdown = {"prettier"},
-            yaml = {"prettier"}
-        },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true
-        }
-    }
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "black" },
+			sh = { "sh" },
+			html = { "prettierd" },
+			css = { "prettierd" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			json = { "prettierd" },
+			markdown = { "prettierd" },
+			yaml = { "prettierd" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
