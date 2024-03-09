63e9b6ce4e87049c1db9e7fd975deca3681d907c
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-08 23:48:36 -0500

    move language related support plugins to language directory

--- nvim/.config/nvim/lua/gavin/lazy.lua
+++ nvim/.config/nvim/lua/gavin/lazy.lua
@@ -4,21 +4,20 @@ vim.g.mapleader = " "
 -- Initializing lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system(
+        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+         lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
-require("lazy").setup("gavin.plugins", {
-	change_detection = {
-		notify = false,
-	},
-	colorscheme = { "github-theme" },
+require("lazy").setup({{
+    import = "gavin.plugins"
+}, {
+    import = "gavin.plugins.language"
+}}, {
+    change_detection = {
+        notify = false
+    },
+    colorscheme = {"github-theme"}
 })
