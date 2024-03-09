82241328240adde044da8097bc4fc3af055b116e
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-09 00:37:15 -0500

    move lsp keymaps to lsp.lua

--- nvim/.config/nvim/lua/gavin/keymaps.lua
+++ nvim/.config/nvim/lua/gavin/keymaps.lua
@@ -17,26 +17,3 @@ map("i", "<S-CR>", "<End>")
 
 -- Stupid <C-c> doesn't trigger InsertLeave
 map("i", "<C-c>", "<Esc>")
-
--- LSP
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(ev)
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = {
-			buffer = ev.buf,
-		}
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-		vim.keymap.set("n", "<leader>d", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "<leader>f", function()
-			vim.lsp.buf.format({
-				async = true,
-			})
-		end, opts)
-	end,
-})
--- nvim/.config/nvim/lua/gavin/plugins/language/lsp.lua
+++ nvim/.config/nvim/lua/gavin/plugins/language/lsp.lua
@@ -36,6 +36,29 @@ return {
 			-- Prolog LSP
 			require("lspconfig").prolog_ls.setup({})
 			vim.cmd("autocmd BufNewFile,BufRead *.pl set filetype=prolog") -- read pl files as prolog not perl
+
+			-- Keymaps
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(ev)
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = {
+						buffer = ev.buf,
+					}
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<leader>d", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "<leader>f", function()
+						vim.lsp.buf.format({
+							async = true,
+						})
+					end, opts)
+				end,
+			})
 		end,
 	},
 }
