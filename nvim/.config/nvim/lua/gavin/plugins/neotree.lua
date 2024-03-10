df85bfd3ab40e463b0868c9143fee86af242824a
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-10 03:29:48 -0400

    further configure neo-tree

--- nvim/.config/nvim/lua/gavin/plugins/neotree.lua
+++ nvim/.config/nvim/lua/gavin/plugins/neotree.lua
@@ -10,9 +10,17 @@ return {
 		require("neo-tree").setup({
 			filesystem = {
 				filtered_items = {
-					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
 					hide_dotfiles = false,
-					hide_gitignored = true,
+					hide_by_name = {
+						".git",
+					},
+					always_show = {
+						".env",
+						".env.local",
+						".env.development",
+						".env.test",
+						".env.production",
+					},
 				},
 			},
 		})
