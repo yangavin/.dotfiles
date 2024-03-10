815be676e3a08befbb34ebe9550d4101c0594e41
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-09 19:20:27 -0500

    integrate telescope to lsp, resolve #4
    also improves find_files function with fd

--- nvim/.config/nvim/lua/gavin/plugins/language/lsp.lua
+++ nvim/.config/nvim/lua/gavin/plugins/language/lsp.lua
@@ -35,13 +35,10 @@ return {
 					local opts = {
 						buffer = ev.buf,
 					}
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 					vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<leader>d", vim.lsp.buf.type_definition, opts)
 					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 					vim.keymap.set("n", "<leader>f", function()
 						vim.lsp.buf.format({
 							async = true,
--- nvim/.config/nvim/lua/gavin/plugins/telescope.lua
+++ nvim/.config/nvim/lua/gavin/plugins/telescope.lua
@@ -1,11 +1,31 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = {"nvim-lua/plenary.nvim"},
-    config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>gf', builtin.live_grep, {})
-        vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-    end
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					-- Respect .gitignore except for .env and show hidden files
+					find_command = {
+						"/bin/sh",
+						"-c",
+						"fd --hidden --type f && fd --hidden --no-ignore .env",
+					},
+				},
+			},
+		})
+
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<C-f>", builtin.find_files, {})
+
+		vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
+		vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols, {})
+
+		vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, {})
+		vim.keymap.set("n", "<leader>gr", builtin.lsp_references, {})
+		vim.keymap.set("n", "<leader>gt", builtin.lsp_type_definitions, {})
+
+		vim.keymap.set("n", "<leader>d", builtin.diagnostics, {})
+	end,
 }
